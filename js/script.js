93a843c5293ed604ec52ef305d037976abdcd05d
EsterSPR <web.esterspreafico@gmail.com>
2022-11-28 18:48:34 +0100

    aggiunto css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const bottone = document.getElementById('generaPass');
 
 bottone.addEventListener('click', function() {
+
+    let nome = document.getElementById('nomePass');
+    let nomeVal = nome.value;
+    document.getElementById('nomePassOut') = nomeVal;
+
     let chilometri = document.getElementById('chilometriPass');
     let chilometriVal = chilometri.value;
     let prezzoNormale =chilometriVal * 0.21;
@@ -13,13 +18,25 @@ bottone.addEventListener('click', function() {
         let prezzoUnder = prezzoNormale / 100 * 20;
         let prezzoUnderRound = prezzoUnder.toFixed(2);
         console.log(prezzoUnderRound);
+        let prezzoUnderRoundFull = prezzoUnderRound + '€';
+        document.getElementById('costoPassOut') = prezzoUnderRoundFull;
+        document.getElementById('offertaPassOut') = "Biglietto Under 18";
     } else if (anniVal > '65') {
         let prezzoOver = prezzoNormale / 100 * 40;
         let prezzoOverRound = prezzoOver.toFixed(2);
         console.log(prezzoOverRound);
+        let prezzoOverRoundFull = prezzoOverRound + '€';
+        document.getElementById('costoPassOut') = prezzoOverRoundFull;
+        document.getElementById('offertaPassOut') = "Biglietto over 65";
     } else {
         let prezzoNormaleRound = prezzoNormale.toFixed(2);
         console.log(prezzoNormaleRound);
+        let prezzoNormaleRoundFull = prezzoNormaleRound + '€';
+        document.getElementById("costoPassOut") = prezzoNormaleRoundFull;
+        document.getElementById('offertaPassOut') = "Biglietto Standard";
     }
 })
 
+function myFunction() {
+    location.reload();
+}
