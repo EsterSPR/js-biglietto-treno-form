6c44527412fa529fcc0dd2f24ec5fd19491ae08f
EsterSPR <web.esterspreafico@gmail.com>
2022-11-30 00:26:52 +0100

    completata seconda milestone

--- js/script.js
+++ js/script.js
@@ -2,38 +2,32 @@ const bottone = document.getElementById('generaPass');
 
 bottone.addEventListener('click', function() {
 
-    let nome = document.getElementById('nomePass');
-    let nomeVal = nome.value;
-    document.getElementById('nomePassOut') = nomeVal;
+    let nome = document.getElementById('nomePass').value;
+    document.getElementById('nomePassOut').innerText = nome;
 
-    let chilometri = document.getElementById('chilometriPass');
-    let chilometriVal = chilometri.value;
-    let prezzoNormale =chilometriVal * 0.21;
+    let chilometri = document.getElementById('chilometriPass').value;
+    let prezzoNormale = chilometri * 0.21;
     console.log(prezzoNormale);
 
-    let anni = document.getElementById('etaPass');
-    let anniVal = anni.value;
+    let anni = document.getElementById('etaPass').value;
 
-    if (anniVal < '18') {
-        let prezzoUnder = prezzoNormale / 100 * 20;
+    if (anni < '18') {
+        let prezzoUnder = prezzoNormale - prezzoNormale / 100 * 20;
         let prezzoUnderRound = prezzoUnder.toFixed(2);
-        console.log(prezzoUnderRound);
         let prezzoUnderRoundFull = prezzoUnderRound + '€';
-        document.getElementById('costoPassOut') = prezzoUnderRoundFull;
-        document.getElementById('offertaPassOut') = "Biglietto Under 18";
-    } else if (anniVal > '65') {
-        let prezzoOver = prezzoNormale / 100 * 40;
+        document.getElementById('costoPassOut').innerText = prezzoUnderRoundFull;
+        document.getElementById('offertaPassOut').innerText = "Biglietto Under 18";
+    } else if (anni > '65') {
+        let prezzoOver = prezzoNormale - prezzoNormale / 100 * 40;
         let prezzoOverRound = prezzoOver.toFixed(2);
-        console.log(prezzoOverRound);
         let prezzoOverRoundFull = prezzoOverRound + '€';
-        document.getElementById('costoPassOut') = prezzoOverRoundFull;
-        document.getElementById('offertaPassOut') = "Biglietto over 65";
+        document.getElementById('costoPassOut').innerText = prezzoOverRoundFull;
+        document.getElementById('offertaPassOut').innerText = "Biglietto over 65";
     } else {
         let prezzoNormaleRound = prezzoNormale.toFixed(2);
-        console.log(prezzoNormaleRound);
         let prezzoNormaleRoundFull = prezzoNormaleRound + '€';
-        document.getElementById("costoPassOut") = prezzoNormaleRoundFull;
-        document.getElementById('offertaPassOut') = "Biglietto Standard";
+        document.getElementById("costoPassOut").innerText = prezzoNormaleRoundFull;
+        document.getElementById('offertaPassOut').innerText = "Biglietto Standard";
     }
 })
 
